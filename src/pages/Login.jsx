141b59f3227794cf1f8009c5353ced58d5ee0cd2
success obayuwana <successobayuwana@gmail.com>
2025-07-05 00:28:12 +0100

    Add new features

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
           <SubmitBtn text="login" />
         </div>
 
-        <button type="button" className="btn btn-secondary btn-block">
+        <button type="button" className="btn btn-secondary btn-block uppercase">
           guest user
         </button>
         <p className="text-center">
